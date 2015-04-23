65d4ca64a7cb46c97a129855ce425a33be932275
Bogdan Kulynych <hello@bogdankulynych.me>
2015-04-23 18:12:30 +0300

    Add serializations test

---
+++ test/test_serializations.cpp
@@ -0,0 +1,33 @@
+#include <sstream>
+
+#include <boost/test/unit_test.hpp>
+
+#include <gmpxx.h>
+
+#include "../src/serializations.hpp"
+#include "serialization_formats.hpp"
+
+using std::stringstream;
+
+
+BOOST_AUTO_TEST_SUITE(SerializationsSuite)
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(mpz_class_serialization, Format, Formats)
+{
+    mpz_class z = 111111111;
+    mpz_class restored_z;
+
+    stringstream ss;
+    {
+        typename Format::oarchive oa(ss);
+        oa << z;
+    }
+    {
+        typename Format::iarchive ia(ss);
+        ia >> restored_z;
+    }
+
+    BOOST_CHECK(z == restored_z);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
