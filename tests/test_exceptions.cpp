ea224724a9b24ae3aaeebfe6c50db3e86d3d7f1c
Bogdan Kulynych <hello@bogdankulynych.me>
2015-04-26 05:30:06 +0300

    Add test for exception

---
+++ tests/test_exceptions.cpp
@@ -0,0 +1,20 @@
+#include <boost/test/unit_test.hpp>
+
+#include "exceptions.hpp"
+
+using she::precondition_not_satisfied;
+
+
+BOOST_AUTO_TEST_SUITE(ExceptionsSuite)
+
+BOOST_AUTO_TEST_CASE(precondition_not_satisfied_construction_and_accessors)
+{
+    try {
+        ASSERT(true == false, "Obviously not.");
+    } catch (const precondition_not_satisfied & e) {
+        BOOST_CHECK_EQUAL(e.what(), "Obviously not. (true == false)");
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
