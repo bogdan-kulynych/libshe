341c88f05a6d930939c04adc7d6cd3c32e83c88d
Bogdan Kulynych <hello@bogdankulynych.me>
2015-05-30 13:47:52 +0300

    Improve PIR benchmark, add total timer

--- benchmarks/pir.cpp
+++ benchmarks/pir.cpp
@@ -9,6 +9,7 @@
 
 using std::cout;
 using std::endl;
+using std::boolalpha;
 using std::vector;
 using std::unique_ptr;
 using std::move;
@@ -50,19 +51,19 @@ randomly_populate_database( vector<PlaintextArray> * database
     }
 }
 
-void build_database_index( vector<PlaintextArray> * database_index
+void build_database_indexes( vector<PlaintextArray> * database_indexes
                          , unsigned int database_size
                          , unsigned int index_size)
 {
     for (unsigned int i = 0; i < database_size; ++i) {
-        database_index->push_back(dec_to_bits(i, index_size));
+        database_indexes->push_back(dec_to_bits(i, index_size));
     }
 }
 
 PrivateKey generate_key( unsigned int security
                        , unsigned int record_size)
 {
-    START_TIMER("KEY GENERATION");
+    START_TIMER("KEY GENERATION", "CLIENT");
 
     const auto params = ParameterSet::generate_parameter_set(security, record_size, 42);
     auto result = PrivateKey(params);
@@ -76,7 +77,7 @@ CompressedCiphertext
 generate_query( const PrivateKey & sk
               , const vector<bool> & index_bits)
 {
-    START_TIMER("QUERY GENERATION");
+    START_TIMER("QUERY GENERATION", "CLIENT");
 
     auto result = sk.encrypt(index_bits);
 
@@ -86,7 +87,7 @@ generate_query( const PrivateKey & sk
 
 EncryptedArray expand_ciphertext(const CompressedCiphertext & ctxt)
 {
-    START_TIMER("QUERY CIPHERTEXT EXPANSION");
+    START_TIMER("QUERY CIPHERTEXT EXPANSION", "SERVER");
 
     auto result = ctxt.expand();
 
@@ -96,11 +97,11 @@ EncryptedArray expand_ciphertext(const CompressedCiphertext & ctxt)
 
 EncryptedArray
 calculate_selection_vector( const EncryptedArray & query
-                          , const vector<PlaintextArray> & database_index)
+                          , const vector<PlaintextArray> & database_indexes)
 {
-    START_TIMER("SELECTION VECTOR HOMOMORPHIC CALCULATION");
+    START_TIMER("SELECTION VECTOR HOMOMORPHIC CALCULATION", "SERVER");
 
-    auto result = query.equal(database_index);
+    auto result = query.equal(database_indexes);
 
     END_TIMER();
     return result;
@@ -110,7 +111,7 @@ EncryptedArray
 calculate_response( const EncryptedArray & sv
                   , const vector<PlaintextArray> & database)
 {
-    START_TIMER("RESPONSE HOMOMORPHIC CALCULATION");
+    START_TIMER("RESPONSE HOMOMORPHIC CALCULATION", "SERVER");
 
     auto result = sv.select(database);
 
@@ -122,7 +123,7 @@ PlaintextArray
 decrypt_response( const PrivateKey & sk
                   , const EncryptedArray & response)
 {
-    START_TIMER("RESPONSE DECRYPTION");
+    START_TIMER("RESPONSE DECRYPTION", "CLIENT");
 
     auto result = sk.decrypt(response);
 
@@ -135,36 +136,70 @@ int main()
 {
     srand (time(NULL));
 
-    unsigned int security = 42;
-    unsigned int database_size = 8;
-    unsigned int record_size = 10;
-    unsigned int index_size = 3;
+    // Security level. 62 for 62-bit security
+    unsigned int security = 62;
+
+    // Number of records in a database
+    unsigned int database_size = 16;
+
+    // Size of every record in bits (note that in general sizes can vary)
+    unsigned int record_size = 64;
+
+    // Size of database index in bits.
+    // To avoid problems should be equal to floor(log2(database_size) + 1
+    unsigned int index_size = 4;
+
+    cout << "Security:      " << security << endl;
+    cout << "Database size: " << database_size << endl;
+    cout << "Record size:   " << record_size << endl;
+    cout << "Index size:    " << record_size << endl << endl;
 
     // Preparation: Generate random database
     vector<PlaintextArray> database;
     randomly_populate_database(&database, database_size, record_size);
 
-    // Preparation: Generate database index
-    vector<PlaintextArray> database_index;
-    build_database_index(&database_index, database_size, index_size);
+    // Preparation: Generate database indexes
+    vector<PlaintextArray> database_indexes;
+    build_database_indexes(&database_indexes, database_size, index_size);
 
     // Generate key
-    auto const sk = move(generate_key(security, record_size));
+    const auto sk = move(generate_key(security, index_size));
 
     // Pick random index and generate compressed query ciphertext
-    auto const ctxt = move(generate_query(sk, random_bits(index_size)));
+    // This is the slowest part. Should consider options to move it to client side.
+    const auto index = rand() % database_size;
+    const auto compressed_ciphertext = move(generate_query(sk, dec_to_bits(index, index_size)));
 
     // Expand the compressed ciphertext
-    auto const query = move(expand_ciphertext(ctxt));
+    const auto encrypted_query = move(expand_ciphertext(compressed_ciphertext));
 
     // Calculate homomorphic selection vector
-    auto const sv = move(calculate_selection_vector(query, database_index));
+    const auto selector = move(calculate_selection_vector(encrypted_query, database_indexes));
 
     // Homomorphically calculate response
-    auto const er = move(calculate_response(sv, database));
+    const auto encrypted_response = move(calculate_response(selector, database));
 
     // Decrypt response
-    auto const pr = move(decrypt_response(sk, er));
+    std::vector<bool> response = move(decrypt_response(sk, encrypted_response));
+
+    TIMER_STATS();
+
+    // Show response if it is small enough
+    if (record_size < 80) {
+        cout << "Queried database index:" << endl;
+        for (const auto & bit : database[index].elements()) {
+            cout << bit;
+        }
+        cout << endl;
+
+        cout << "Obtained result:" << endl;
+        for (const auto & bit : response) {
+            cout << bit;
+        }
+        cout << endl;
+    }
+
+    cout << "Correct result? " << boolalpha << (database[index] == response) << endl;
 
     return 0;
 }
--- benchmarks/utils.hpp
+++ benchmarks/utils.hpp
@@ -1,15 +1,37 @@
 #include <ctime>
+#include <map>
+#include <string>
 #include <chrono>
 #include <iostream>
 
 
-#define START_TIMER( section ) \
+using chrono_t = std::chrono::duration<double>;
+std::map<std::string, chrono_t> _timer_global;
+
+
+#define START_TIMER( section, group ) \
+        _timer_global.emplace(std::pair<std::string, chrono_t>(group, {})); \
+        std::string _timer_group = group; \
         std::cout << "> " << section << std::endl; \
         std::chrono::time_point<std::chrono::system_clock> _timer_start, _timer_end; \
-        _timer_start = std::chrono::system_clock::now(); \
+        _timer_start = std::chrono::system_clock::now();
 
 #define END_TIMER() \
         _timer_end = std::chrono::system_clock::now(); \
-        std::chrono::duration<double> elapsed_seconds = _timer_end - _timer_start; \
-        std::cout << "< Secs: " << elapsed_seconds.count() << std::endl; \
+        chrono_t elapsed_seconds = _timer_end - _timer_start; \
+        _timer_global[_timer_group] += elapsed_seconds; \
+        std::cout << "< Seconds: " << elapsed_seconds.count() << std::endl; \
+        std::cout << std::endl;
+
+#define TIMER_STATS() \
+        chrono_t _total_seconds {}; \
+        for (const auto & entry : _timer_global) { \
+            const auto & group = entry.first; \
+            const auto & elapsed_seconds = entry.second; \
+            std::cout << "Group " << group << ". "; \
+            std::cout << "Seconds: " << elapsed_seconds.count() << std::endl; \
+            _total_seconds += elapsed_seconds; \
+        } \
+        std::cout << "-----" << std::endl; \
+        std::cout << "Total seconds: " << _total_seconds.count() << std::endl; \
         std::cout << std::endl;
